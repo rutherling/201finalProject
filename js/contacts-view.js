b7f5e40c2b4fe3bb19e1b3db5b699e09fc5a5e16
libertinistic <libertinistic@gmail.com>
2016-06-01 11:42:52 -0700

    rough logic for view functions on overdue and contact pages

---
+++ js/contacts-view.js
@@ -0,0 +1,13 @@
+//Any functions that affect what the users see are here
+
+//Previous step: contactArray.push(submitObject)
+//Input: index from contactArray
+//Output: single <div> that holds everything
+//then for loop that appends them all to document.body
+
+for (var i = 0; i < listAlphabetical().length; i++) {
+  var singleBubble = labelMaker(listAlphabetical()[i].topic,
+    listAlphabetical()[i].firstName,
+    listAlphabetical()[i].lastName);
+  document.getElementById('contactList').appendChild(singleBubble);
+}
