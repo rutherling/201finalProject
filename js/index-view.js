38020abc787f0d008d6329303acb20ec3022a034
libertinistic <libertinistic@gmail.com>
2016-06-02 00:41:30 -0700

    added eventListeners for overdueList action buttons. added odL doneButton

--- js/index-view.js
+++ js/index-view.js
@@ -5,11 +5,6 @@
 //Output: single <div> that holds everything
 //then for loop that appends them all to document.body
 
-for (var i = 0; i < listNextDates().length; i++) {
-  var singleBubble = labelMaker(listNextDates()[i], '');
-  document.getElementById('timeline').appendChild(singleBubble);
-}
-
 // Populate overdue tray
 for (var i = 0; i < listOverdues().length; i++) {
   //stop displaying new bubbles and link to overdue view
--- js/labelMaker.js
+++ js/labelMaker.js
@@ -1,9 +1,3 @@
-// document.addEventListener('click', function() {
-//   // console.log(event.target.ctctLabels.id);
-//   event.stopPropagation();
-//   console.log(this.id);
-// }, false);
-
 function labelMaker (contactObject, status) {
   //Make div to display contact topic excerpt
   console.log('Label Maker is running.');
@@ -59,10 +53,24 @@ function labelMaker (contactObject, status) {
     postDiv.setAttribute('class', 'postponeButton btn btn-default btn-lg');
     postDiv.textContent = 'Postpone';
     innerDiv.appendChild(postDiv);
+    postDiv.addEventListener('click', function() {
+      console.log('postponeContact called');
+    }, false);
+  //Make done button
+    var doneDiv = document.createElement('div');
+    doneDiv.setAttribute('class', 'doneButton btn btn-default btn-lg');
+    doneDiv.textContent = 'Complete';
+    innerDiv.appendChild(doneDiv);
+    postDiv.addEventListener('click', function() {
+      console.log('doneContact called');
+    }, false);
   //Make remove button if status is overdueList
     var killDiv = document.createElement('i');
     killDiv.setAttribute('class', 'fa fa-times-circle');
     innerDiv.appendChild(killDiv);
+    killDiv.addEventListener('click', function() {
+      console.log('removeContact called');
+    }, false);
   }
 
   //Make containing div for contact label
